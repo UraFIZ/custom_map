bfa675b3b736995e085377c411eb4da48f6150f9
UraFIZ <kratser88@gmail.com>
2020-05-18 15:12:22 +0300

    added pins

--- script.js
+++ script.js
@@ -2,145 +2,211 @@ const plus = document.querySelector("div.tool.plus");
 const minus = document.querySelector("div.tool.minus");
 const refreshbtn = document.querySelector(".refresh")
 const map = document.querySelector(".map_img");
-//pins
-const pins = document.querySelectorAll(".pin");
-const pin_mountings = document.querySelector(".pin_mountings");
-const pin_close = document.querySelector(".pin_red--closer");
-const pin_elevator = document.querySelector(".pin_elevator");
-const pin_bag = document.querySelector(".pin_bag");
-const pin_in = document.querySelector(".pin_in-jan");
-const pin_farther = document.querySelector(".pin_red--farther");
-const pin_rhomb = document.querySelector(".pin_rhomb")
+const printContainer = document.querySelector('.pin-container');
 
+let ratio = 0.322;
 //modal
 const modal = document.querySelector(".modal");
 
 
-pins.forEach((item) => {
-    item.addEventListener("mouseover", function (e) {
-        e.preventDefault();
-        e.stopPropagation();
-        let x = e.clientX - e.target.offsetLeft;
-        let y = e.clientY - e.target.offsetTop - 50;
-        const first = this.querySelector("span .first").innerText;
-        const second = this.querySelector("span .second").innerText;
-        modal.querySelector(".first-p").innerText = first;
-        modal.querySelector(".second-p").innerText = second
-        modal.style.display = "block";
-        modal.style.top = `${y}px`
-        modal.style.left = `${e.clientX - 60}px`
+// pins.forEach((item) => {
+//     item.addEventListener("mouseover", function (e) {
+//         e.preventDefault();
+//         e.stopPropagation();
+//         let x = e.clientX - e.target.offsetLeft;
+//         let y = e.clientY - e.target.offsetTop - 50;
+//         const first = this.querySelector("span .first").innerText;
+//         const second = this.querySelector("span .second").innerText;
+//         modal.querySelector(".first-p").innerText = first;
+//         modal.querySelector(".second-p").innerText = second
+//         modal.style.display = "block";
+//         modal.style.top = `${y}px`
+//         modal.style.left = `${e.clientX - 60}px`
 
-    })
-    item.addEventListener("mouseout", function (e) {
-        modal.style.display = "none";
-    })
-})
+//     })
+//     item.addEventListener("mouseout", function (e) {
+//         modal.style.display = "none";
+//     })
+// })
 
 
 function zoomIn() {
-    let step = 273;
-    let topStep = -75
-    let leftStep = -120;
-    plus.addEventListener("click", function () {
-        map.style.width = `${map.clientWidth + step}px`
-        map.style.top = `${map.offsetTop + topStep}px`
-        map.style.left = `${map.offsetLeft + leftStep}px`
-        changePinsPosition("zoomIn");
-
+    plus.addEventListener("click", function (e) {
+        e.preventDefault();
+       let imgOffsetLeft = $('.map_img').css('left');
+       let imgOffsetTop = $('.map_img').css('top');
+       if (ratio < 1.6) {
+          ratio += 0.12
+       } else {
+           ratio = 1.6;
+       }
+        setOriginalResidenceMarker(imgOffsetLeft, imgOffsetTop, ratio, true)
+        needToRedraw();
     })
 }
 zoomIn()
 
 function zoomOut() {
-    let step = 273;
-    let topStep = 75;
-    let leftStep = 120;
-    minus.addEventListener("click", function () {
-        if (map.clientWidth > 881) {
-            map.style.width = `${map.clientWidth - step}px`
-            map.style.top = `${map.offsetTop + topStep}px`
-            map.style.left = `${map.offsetLeft + leftStep}px`
-            changePinsPosition("zoomOut")
+    minus.addEventListener("click", function (e) {
+        e.preventDefault();
+        if (ratio > 0.33) {
+            ratio -= 0.12;
+
+        } else {
+            ratio = 0.322;
         }
+        let imgOffsetLeft = $('.map_img').css('left');
+        let imgOffsetTop = $('.map_img').css('top');
+        setOriginalResidenceMarker(imgOffsetLeft, imgOffsetTop, ratio, true)
+        needToRedraw();
     })
 }
 zoomOut()
 
 function refresh() {
-    refreshbtn.addEventListener("click", function () {
-        map.style.width = "880px";
-        map.style.top = "0px";
-        map.style.left = "0px";
-        changePinsPosition();
+    refreshbtn.addEventListener("click", function (e) {
+        e.preventDefault();
+        ratio = 0.322;
+        setOriginalResidenceMarker(0, 0, ratio, false)
+        needToRedraw();
     })
 }
 refresh()
 
-function changePinsPosition(arg) {
-    if (arg == "zoomIn") {
-        pin_mountings.style.top = `${pin_mountings.offsetTop - 9}px`
-        pin_mountings.style.left = `${pin_mountings.offsetLeft + 51}px`
 
-        pin_close.style.top = `${pin_close.offsetTop - 1}px`
-        pin_close.style.left = `${pin_close.offsetLeft + 52}px`
 
-        pin_elevator.style.top = `${pin_elevator.offsetTop - 20}px`
-        pin_elevator.style.left = `${pin_elevator.offsetLeft + 54}px`
 
-        pin_bag.style.top = `${pin_bag.offsetTop - 28}px`
-        pin_bag.style.left = `${pin_bag.offsetLeft + 39}px`
+$( function() {
+    $( ".map_img" ).draggable();
+  } );
 
-        pin_in.style.top = `${pin_in.offsetTop - 18}px`
-        pin_in.style.left = `${pin_in.offsetLeft + 33}px`
+  $( function() {
+    $( ".map_img" ).draggable({
+      start: function() {
+        $('.pin').fadeOut(200);
+      },
+      stop: function() {
+        needToRedraw();
+      }
+    });
+ 
+  } );
 
-        pin_farther.style.top = `${pin_farther.offsetTop - 17}px`
-        pin_farther.style.left = `${pin_farther.offsetLeft + 22}px`
+  function createPins(data) {
+    pinDiv = document.createElement("div");
+    pinDiv.classList.add('pin');
 
-        pin_rhomb.style.top = `${pin_rhomb.offsetTop - 19}px`
-        pin_rhomb.style.left = `${pin_rhomb.offsetLeft + 15}px`
-    } else if (arg == "zoomOut") {
-        pin_mountings.style.top = `${pin_mountings.offsetTop + 9}px`
-        pin_mountings.style.left = `${pin_mountings.offsetLeft - 51}px`
+        $(pinDiv).attr("native-left", data.coordinateX);
+        $(pinDiv).attr("native-top", data.coordinateY);
+        pinDiv.innrHtml = `
+        <span>
+            <p>${data.title}</p> <br/>
+            <p>${data.description}</p>
+        </span>
+        `   
+        var ratio = 880 / 2732;
+        var newLeft = ((data.coordinateX * +ratio.toFixed(3)) - 22);
+        var newTop = ((data.coordinateY * +ratio.toFixed(3)) - 65) + 11;
+        $(pinDiv).css({
+            'top': (newTop) + 'px',
+            'left': (newLeft) + 'px'
+        })
+        printContainer.append(pinDiv)
 
-        pin_close.style.top = `${pin_close.offsetTop + 1}px`
-        pin_close.style.left = `${pin_close.offsetLeft - 52}px`
+  }
+  Data.poiData.forEach(createPins)
+  
 
-        pin_elevator.style.top = `${pin_elevator.offsetTop + 20}px`
-        pin_elevator.style.left = `${pin_elevator.offsetLeft - 54}px`
+  function setOriginalResidenceMarker(refX, refY, ratio, refresh) {
+    ratio =  +ratio.toFixed(3)
+    console.log(ratio)
+    var x = parseInt(refX);
+    var y = parseInt(refY);
+    var deltaWidth = 0;
+    var deltaHeight = 0;
+    var oldWidth = $('.map_img').width();
+    var oldHeight = $('.map_img').height();
+    var newWidth = 2732 * ratio;
+    var newHeight = 1536 * ratio;
 
-        pin_bag.style.top = `${pin_bag.offsetTop + 28}px`
-        pin_bag.style.left = `${pin_bag.offsetLeft - 39}px`
-
-        pin_in.style.top = `${pin_in.offsetTop + 18}px`
-        pin_in.style.left = `${pin_in.offsetLeft - 33}px`
-
-        pin_farther.style.top = `${pin_farther.offsetTop + 17}px`
-        pin_farther.style.left = `${pin_farther.offsetLeft - 22}px`
-
-        pin_rhomb.style.top = `${pin_rhomb.offsetTop + 19}px`
-        pin_rhomb.style.left = `${pin_rhomb.offsetLeft - 15}px`
-    } else {
-        pin_mountings.style.top = "148.808px"
-        pin_mountings.style.left = "534.738px"
-
-        pin_close.style.top = "194.398px"
-        pin_close.style.left = "536.53px"
-
-        pin_elevator.style.top = "126.778px"
-        pin_elevator.style.left = "531.378px"
-
-        pin_bag.style.top = "87.494px"
-        pin_bag.style.left = "478.57px"
-
-        pin_in.style.top = "124.202px"
-        pin_in.style.left = "470.198px"
-
-        pin_farther.style.top = "129.998px"
-        pin_farther.style.left = "437.354px"
-
-        pin_rhomb.style.top = "122.27px"
-        pin_rhomb.style.left = "414.17px"
+    if (refresh) {
+        deltaWidth = (oldWidth - newWidth) / 2;
+        deltaHeight = (oldHeight - newHeight) / 2;
     }
+    $('.map_img').width(newWidth);
+    var offsetLeft = x + deltaWidth;
+    var offsetTop = y + deltaHeight;
+    $('.map_img').css({
+        "top": offsetTop,
+        "left": offsetLeft
+    });
+    $('.pin').each(function() {
+        var residenceMarker = $(this);
+        var addTop = 65;
+        var addLeft = 22;
+        if (residenceMarker.hasClass('marker_poi')) {
+            var addTop = 56;
+            var addLeft = 21;
+        }
+    
+        var originalLeft = (residenceMarker.attr('native-left') * ratio) + deltaWidth - addLeft + x;
+        var originalTop = (residenceMarker.attr('native-top') * ratio) + deltaHeight - addTop + y;
+        residenceMarker.css({
+            'top': (originalTop) + 'px',
+            'left': (originalLeft) + 'px'
+        });
+    });
+}
 
+function needToRedraw() {
+    imgOffsetLeft = $('.map_img').css("left");
+    imgOffsetTop = $('.map_img').css("top");
+    var x = parseInt(imgOffsetLeft);
+    var y = parseInt(imgOffsetTop);
+    var cadreWidth = parseInt($('.map_wrapper').width());
+    var imgWidth = parseInt($('.map_img').width());
+    var deltaX = imgWidth - cadreWidth + x;
+    var cadreHeight = parseInt($('.map_wrapper').height());
+    var imgHeight = parseInt($('.map_img').height());
+    var deltaY = imgHeight - cadreHeight + y;
+    var newX = x;
+    var newY = y;
+    if (x > 0 || y > 0 || deltaX < 0 || deltaY < 0) {
+        if (x > 0) {
+            newX = 0;
+        }
+        if (y > 0) {
+            newY = 0;
+        }
+        if (deltaX < 0) {
+            newX = cadreWidth - imgWidth;
+        }
+        if (deltaY < 0) {
+            newY = cadreHeight - imgHeight;
+        }
+        $('.map_img').css('z-index', 4);
+        $('.map_img').animate({
+            top: newY,
+            left: newX
+        }, {
+            queue: false,
+            duration: 0,
+            complete: function() {
+                imgOffsetLeft = newX;
+                imgOffsetTop = newY;
+                setOriginalResidenceMarker(imgOffsetLeft, imgOffsetTop, ratio, true);
+                $('.pin').fadeIn(200);
+                $('.pin').css({
+                    'display': ''
+                });
+            }
+        });
+    } else {
+        setOriginalResidenceMarker(imgOffsetLeft, imgOffsetTop, ratio, true);
+        $('.pin').fadeIn(200);
+    }
+    $('.pin').css({
+        'display': ''
+    });
 }
 
